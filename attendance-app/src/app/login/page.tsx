9a38977d79c045c9a0d06b4ebb919744cdc20499
Naman13112004 <njudaipur04@gmail.com>
2026-01-28 17:51:36 +0530

    adding suspense to login page

--- attendance-app/src/app/login/page.tsx
+++ attendance-app/src/app/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginTabs from "@/components/auth/login-tabs";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] bg-muted/40 px-4 py-8">
-      <LoginTabs />
+      <Suspense fallback={<p>Loading...</p>}>
+        <LoginTabs />
+      </Suspense>
     </div>
   );
 }
